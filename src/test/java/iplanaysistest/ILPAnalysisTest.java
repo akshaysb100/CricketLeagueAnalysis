7818cd5cd520d37d65b2e68298c38974915367ba
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-14 15:02:15 +0530

    Add:Create test case if pass wrong delimiter then throw exception

--- src/test/java/iplanaysistest/ILPAnalysisTest.java
+++ src/test/java/iplanaysistest/ILPAnalysisTest.java
@@ -1,6 +1,5 @@
 package iplanaysistest;
 
-import com.csvbuilder.CsvBuilderException;
 import cricketleagueanalysis.IPLAnalysis;
 import cricketleagueanalysis.IPLException;
 import org.junit.Assert;
@@ -10,9 +9,10 @@ public class ILPAnalysisTest {
 
     private static final String IPL_MOST_RUN_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String WRONG_DELIMITER_CSV_FILE_PATH = "./src/test/resources/WrongDelimiterIPL2019MostRun.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
 
     @Test
-    public void givenIPLMostRunFile_ShouldGetMapOfCorrectSize() {
+    public void givenIPLMostRunCSVFile_ShouldGetMapOfCorrectSize() {
         IPLAnalysis iplAnalysis = new IPLAnalysis();
         int numOfRecords = 0;
         try {
@@ -24,11 +24,23 @@ public class ILPAnalysisTest {
     }
 
     @Test
-    public void givenIPLMostRunFile_WithWrongDelimiter_ShouldThrowException() {
+    public void givenIPLMostRunCSVFile_WithWrongDelimiter_ShouldThrowException() {
         IPLAnalysis iplAnalysis = new IPLAnalysis();
         int numOfRecords = 0;
         try {
             numOfRecords = iplAnalysis.loadIPLCSVFileData(WRONG_DELIMITER_CSV_FILE_PATH);
+            Assert.assertEquals(100, numOfRecords);
+        } catch (IPLException e) {
+            Assert.assertEquals(IPLException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunCSVFile_WithWrong_ShouldThrowException() {
+        IPLAnalysis iplAnalysis = new IPLAnalysis();
+        int numOfRecords = 0;
+        try {
+            numOfRecords = iplAnalysis.loadIPLCSVFileData(WRONG_CSV_FILE_PATH);
             Assert.assertEquals(100, numOfRecords);
         } catch (IPLException e) {
             Assert.assertEquals(IPLException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
