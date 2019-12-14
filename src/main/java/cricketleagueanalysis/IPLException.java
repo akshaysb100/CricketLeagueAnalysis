89c2d4ab2e77ef6b1c2c51b239a176628727303f
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-14 14:54:58 +0530

    Add:Create test case if pass wrong file then throw exception

---
+++ src/main/java/cricketleagueanalysis/IPLException.java
@@ -0,0 +1,4 @@
+package cricketleagueanalysis;
+
+public class IPLException {
+}
--- src/test/java/iplanaysistest/ILPAnalysisTest.java
+++ src/test/java/iplanaysistest/ILPAnalysisTest.java
@@ -1,17 +1,37 @@
 package iplanaysistest;
 
+import com.csvbuilder.CsvBuilderException;
 import cricketleagueanalysis.IPLAnalysis;
+import cricketleagueanalysis.IPLException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ILPAnalysisTest {
 
     private static final String IPL_MOST_RUN_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String WRONG_DELIMITER_CSV_FILE_PATH = "./src/test/resources/WrongDelimiterIPL2019MostRun.csv";
 
     @Test
     public void givenIPLMostRunFile_ShouldGetMapOfCorrectSize() {
         IPLAnalysis iplAnalysis = new IPLAnalysis();
-        int numOfRecords = iplAnalysis.loadIPLCSVFileData(IPL_MOST_RUN_CSV_FILE_PATH);
-        Assert.assertEquals(100,numOfRecords);
+        int numOfRecords = 0;
+        try {
+            numOfRecords = iplAnalysis.loadIPLCSVFileData(IPL_MOST_RUN_CSV_FILE_PATH);
+            Assert.assertEquals(100, numOfRecords);
+        } catch (IPLException e) {
+            Assert.assertEquals(IPLException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunFile_WithWrongDelimiter_ShouldThrowException() {
+        IPLAnalysis iplAnalysis = new IPLAnalysis();
+        int numOfRecords = 0;
+        try {
+            numOfRecords = iplAnalysis.loadIPLCSVFileData(WRONG_DELIMITER_CSV_FILE_PATH);
+            Assert.assertEquals(100, numOfRecords);
+        } catch (IPLException e) {
+            Assert.assertEquals(IPLException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
     }
 }
