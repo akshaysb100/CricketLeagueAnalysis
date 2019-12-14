55f5ccf9d23085d9eeeb30e6581b5dc1fef1ed81
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-14 13:24:20 +0530

    Add:Create test case  how much number of record in ipl run csv file

--- src/test/java/iplanaysistest/ILPAnalysisTest.java
+++ src/test/java/iplanaysistest/ILPAnalysisTest.java
@@ -1,4 +1,17 @@
 package iplanaysistest;
 
-public class ILPSnaysisTest {
+import cricketleagueanalysis.IPLAnalysis;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ILPAnalysisTest {
+
+    private static final String IPL_MOST_RUN_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+
+    @Test
+    public void givenIPLMostRunFile_ShouldGetMapOfCorrectSize() {
+        IPLAnalysis iplAnalysis = new IPLAnalysis();
+        int numOfRecords = iplAnalysis.loadIPLCSVFileData(IPL_MOST_RUN_CSV_FILE_PATH);
+        Assert.assertEquals(100,numOfRecords);
+    }
 }
