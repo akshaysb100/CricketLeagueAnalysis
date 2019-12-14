d94e0a8a4adb4d787a7894aaac78f191d1486475
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-14 17:32:34 +0530

    Add:Created function for get best avreage batting palyer name

--- src/main/java/cricketleagueanalysis/IPLAnalysis.java
+++ src/main/java/cricketleagueanalysis/IPLAnalysis.java
@@ -3,18 +3,25 @@ package cricketleagueanalysis;
 import com.csvbuilder.CSVBuilderFactory;
 import com.csvbuilder.CsvBuilderException;
 import com.csvbuilder.ICSVBuilder;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.StreamSupport;
 
+import static java.util.stream.Collectors.toCollection;
+
 public class IPLAnalysis {
-    Map<String, IPLMostRunCsvData> analysisMap = new HashMap<String, IPLMostRunCsvData>();
+    Map<String, IPLAnalysisDAO> analysisMap = new HashMap<String, IPLAnalysisDAO>();
+    Map<SortedDataBaseOnField, Comparator<IPLAnalysisDAO>> fields = null;
+
+    public IPLAnalysis() {
+        this.fields = new HashMap<>();
+        this.fields.put(SortedDataBaseOnField.AVERAGE, Comparator.comparing(census -> census.average,Comparator.reverseOrder()));
+    }
 
     public int loadIPLCSVFileData(String iplMostRunCsvFilePath) throws IPLException {
         try (Reader reader = Files.newBufferedReader(Paths.get(iplMostRunCsvFilePath))) {
@@ -23,7 +30,7 @@ public class IPLAnalysis {
             Iterable<IPLMostRunCsvData> indiaCensusCSVS = () -> censusList;
             StreamSupport.stream(indiaCensusCSVS.spliterator(), false)
                     .map(IPLMostRunCsvData.class::cast)
-                    .forEach(csvCensus -> analysisMap.put(csvCensus.playerName, new IPLMostRunCsvData(csvCensus)));
+                    .forEach(csvCensus -> analysisMap.put(csvCensus.playerName, new IPLAnalysisDAO(csvCensus)));
         } catch (IOException e) {
             throw new IPLException(e.getMessage(),
                     IPLException.ExceptionType.WRONG_FILE_PATH);
@@ -32,5 +39,17 @@ public class IPLAnalysis {
                     IPLException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
         return analysisMap.size();
+    }
+
+    public String getTopAverageBattingPlayerName(SortedDataBaseOnField fieldName) throws IPLException {
+        if (analysisMap == null || analysisMap.size() == 0) {
+            throw new IPLException("No Data", IPLException
+                    .ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+        ArrayList arrayList = analysisMap.values().stream()
+                .sorted(this.fields.get(fieldName))
+                .collect(toCollection(ArrayList::new));
+        String sortedStateCensus = new Gson().toJson(arrayList);
+        return sortedStateCensus;
     }
 }
